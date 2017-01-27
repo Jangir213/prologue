d158170d422d0e8f214210485b3f6708a858eb2a
jangir <jangirhus@gmail.com>
2017-01-27 18:05:12 +0200

    add mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,34 @@
 
   window.addEventListener('load', function () {
     $('.scrollspy').scrollSpy({ scrollOffset: 0 });
+
+    mobileMenu();
   });
 
+  function mobileMenu() {
+    var btn = document.querySelector('.mobile-menu__btn');
+    var cover = document.querySelector('.mobile-menu__cover');
+    var aside = document.querySelector('.aside');
+    var main = document.querySelector('.main');
+
+    btn.addEventListener('click', toggleMenu);
+    cover.addEventListener('click', toggleMenu);
+
+    function toggleMenu() {
+      if (aside.style.transform === 'translateX(275px)') {
+        aside.style.transform = '';
+        main.style.transform = '';
+        btn.style.transform = '';
+        cover.style.display = '';
+      } else {
+        aside.style.transform = 'translateX(275px)';
+        main.style.transform = 'translateX(275px)';
+        btn.style.transform = 'translateX(275px)';
+        cover.style.display = 'block';
+      }
+    }
+
+
+  }
 
 })();
